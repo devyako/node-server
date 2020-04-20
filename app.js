90594d59222cbcec39b9c36950e367c75ecd1cee
devyako <yakhyoismoildjonov@gmail.com>
2020-04-20 11:28:13 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+});
+
+server.listen(3000);
