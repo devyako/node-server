ae50058e0efc291558193d77fa4ae47490538801
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-08-30 15:51:22 +0500

    events

---
+++ events.js
@@ -0,0 +1,9 @@
+const events = require("events");
+
+const emitter = new events.EventEmitter();
+
+emitter.on("customEvent", (message, user) => {
+  console.log(`${user}: ${message}`);
+});
+
+emitter.emit("customEvent", "Hello World", "Computer");
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require("http");
 const routes = require("./routes");
+const name = require("./events");
+
+console.log(name);
 
 const server = http.createServer(routes);
 server.listen(3000);
